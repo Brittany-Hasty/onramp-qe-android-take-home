f5dcd67c5c9eafcc13dc717ed1091c43c763ffe1
Brittany Hasty <brittany.r.hasty@gmail.com>
2021-03-02 18:08:36 -0800

    Narrow down test scope

---
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/AddImagesToNotecardsTest.java
@@ -0,0 +1,16 @@
+package com.sxmp.apprentice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddImagesToNotecardsTest {
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void tempName() {
+
+    }
+}
---
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/AddStylesToNotecardsTest.java
@@ -0,0 +1,16 @@
+package com.sxmp.apprentice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddStylesToNotecardsTest {
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void tempName() {
+
+    }
+}
