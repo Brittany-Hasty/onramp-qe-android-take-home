9c36f20931274cff61917b87f3f6847ee0dc6a8f
Brittany Hasty <brittany.r.hasty@gmail.com>
2021-03-04 14:14:48 -0800

    Add test function outlines to AddNoteCardTypesTest

--- AnkiDroid/src/androidTest/java/com/sxmp/apprentice/AddNoteCardTypesTest.java
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/AddNoteCardTypesTest.java
@@ -1,19 +1,152 @@
 package com.sxmp.apprentice;
 
+import com.ichi2.anki.DeckPicker;
+import com.ichi2.anki.R;
+
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 public class AddNoteCardTypesTest {
+    private ActivityTestRule<DeckPicker> activityRule =
+            new ActivityTestRule<>(DeckPicker.class, false, false);
+
+    private GrantPermissionRule grantRule =
+            GrantPermissionRule.grant(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE);
+
+    @Rule
+    public RuleChain ruleChain = RuleChain.outerRule(grantRule).around(activityRule);
+
     @Before
     public void setup() {
         // App needs to be open
-        // A deck needs to exist
-        // A card must be open to the add/edit form
+        activityRule.launchActivity(null);
+
+        // Open the action menu
+    }
+
+    @Test
+    public void manageNoteTypesAppearsInActionMenuTest() {
 
     }
 
     @Test
-    public void tempName() {
+    public void manageNoteTypesIsClickableTest() {
+
+    }
+
+    @Test
+    public void manageNoteTypesClickDisplaysNoteTypesListTest() {
+
+    }
+
+    @Test
+    public void noteTypesListContainsTextManageNoteTypesTest() {
+
+    }
+
+    @Test
+    public void noteTypesListContainsTotalNumberOfTypesTest() {
+
+    }
+
+    @Test
+    public void noteTypesListContainsTypesTest() {
+        // Look for 'Basic', it is a default type that will exist
+    }
+
+    @Test
+    public void noteTypesListContainsActionMenuTest() {
+
+    }
+
+    @Test
+    public void typeListActionMenuDisplayedAsPlusIconTest() {
+
+    }
+
+    @Test
+    public void typeListActionMenuIsClickableTest() {
+
+    }
+
+    @Test
+    public void typeListActionMenuClickBringsUpNewTypeFormTest() {
+
+    }
+
+    @Test
+    public void newTypeFormContainsTextAddNoteTypeTest() {
+
+    }
+
+    @Test
+    public void newTypeFormContainsCancelButtonTest() {
+
+    }
+
+    @Test
+    public void newTypeFormCancelButtonIsClickableTest() {
+
+    }
+
+    @Test
+    public void newTypeFormCancelButtonClickReturnsToTypeListTest() {
+
+    }
+
+    @Test
+    public void newTypeFormContainsOkButtonTest() {
+
+    }
+
+    @Test
+    public void newTypeFormOkButtonIsClickableTest() {
+
+    }
+
+    @Test
+    public void newTypeFormOkButtonClickReturnsToTypeListTest() {
+
+    }
+
+    @Test
+    public void newTypeAddedToTypeListOnCreationTest() {
+
+    }
+
+    @Test
+    public void newTypeFormContainsCloneDropdownTest() {
+
+    }
+
+    @Test
+    public void newTypeFormCloneDropdownDefaultIsBasicTest() {
+
+    }
+
+    @Test
+    public void typeListContainsTypesTotalNotesTest() {
+
+    }
+
+    @Test
+    public void newlyCreatedTypeCanBeAddedToCardTest() {
 
     }
 }
