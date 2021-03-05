f2c5d7cdd0dd2a8fc11fdf28173f5f0697494ca5
Brittany Hasty <brittany.r.hasty@gmail.com>
2021-03-04 21:38:23 -0800

    Add test function outlines to EditNoteCardTypeStyleTest

--- AnkiDroid/src/androidTest/java/com/sxmp/apprentice/EditNoteCardTypeStyleTest.java
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/EditNoteCardTypeStyleTest.java
@@ -13,7 +13,77 @@ public class EditNoteCardTypeStyleTest {
     }
 
     @Test
-    public void Test() {
+    public void canSeeStyleByClickingCardsOnAddNoteFormTest() {
+
+    }
+
+    @Test
+    public void cardStyleIncludesFrontTemplateTextTest() {
+
+    }
+
+    @Test
+    public void cardStyleIncludesBackTemplateTextTest() {
+
+    }
+
+    @Test
+    public void cardStyleIncludesStylingTextTest() {
+
+    }
+
+    @Test
+    public void defaultStylingExistsOnFormTest() {
+
+    }
+
+    @Test
+    public void defaultStylingCanBeReplacedTest() {
+
+    }
+
+    @Test
+    public void defaultFrontTemplateExistsTest() {
+
+    }
+
+    @Test
+    public void defaultFrontTemplateCanBeReplacedTest() {
+
+    }
+
+    @Test
+    public void defaultBackTemplateExistsTest() {
+
+    }
+
+    @Test
+    public void defaultBackTemplateCanBeReplacedTest() {
+
+    }
+
+    @Test
+    public void changingFontFamilyAppearsInCardTest() {
+
+    }
+
+    @Test
+    public void changingFontSizeAppearsInCardTest() {
+
+    }
+
+    @Test
+    public void changingTextAlignmentAppearsInCardTest() {
+
+    }
+
+    @Test
+    public void changingColorAppearsInCardTest() {
+
+    }
+
+    @Test
+    public void changingBackgroundColorAppearsInCardTest() {
 
     }
 }
